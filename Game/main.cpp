cdcdae121c8b18cf46ce575e34c4147700622857
FullMetalPie <andrea.pietri06@gmail.com>
2024-01-24 15:00:21 +0100

    updated

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include "imgui.h"
+#include "../imgui-master/imgui.h"
+#include "../imgui-master/imconfig.h"
+#include "../imgui-master/backends/imgui_impl_opengl3.h"
+#include "../imgui-master/backends/imgui_impl_sdl2.h"
 
 using namespace std;
 
 int main() {
-    cout << "Ciao";
+    ImGui::ShowDemoWindow();
     return 0;
 }
