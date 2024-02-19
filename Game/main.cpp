13017d546e875b0c86c4e4f53b29bd0563a1095d
FullMetalPie <andrea.pietri06@gmail.com>
2024-02-19 17:22:27 +0100

    Updated main file

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-#include "../imgui-master/imgui.h"
-#include "../imgui-master/imconfig.h"
-#include "../imgui-master/backends/imgui_impl_opengl3.h"
-#include "../imgui-master/backends/imgui_impl_sdl2.h"
 
 using namespace std;
 
 int main() {
-    ImGui::ShowDemoWindow();
     return 0;
 }
