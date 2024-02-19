3100e9ce225b833681c531db41d8b31f8127beda
FullMetalPie <andrea.pietri06@gmail.com>
2024-02-19 17:59:56 +0100

    Created Deck class files (unfinished)

---
+++ Deck.h
@@ -0,0 +1,24 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <iostream>
+#include "Card.h"
+#define MAX_CARTE
+
+using namespace std;
+
+class Deck {
+    private:
+        Card* cardsArray;
+        int dim;
+    public:
+        /*COSTRUTTORE e DISTRUTTORE*/
+        Deck();
+        ~Deck();
+        /*SETTER e GETTER*/
+        int getDim();
+
+        void setDim(int dim);
+};
+
+#endif
